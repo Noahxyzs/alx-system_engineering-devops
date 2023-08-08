ab433266faecec58f3cfa2019397ddbeaaedb274
Noahxyzs <noahsolomon528@yahoo.com>
2023-08-08 10:46:03 -0700

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Queries reddit for a subreddit
-and prints the titles of the first 10 hot posts
-listed inside it
-data.children.data.title
+"""
+Queries the Reddit API and prints the first ten hot posts for a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
-    """Returns the titles of top 10 posts for a subreddit"""
-    url = "https://www.reddit.com/r/{}/best.json".format(subreddit)
-
-    response = requests.get(url, allow_redirects=False)
-    if response.status_code == 404:
-        print(None)
-        return
-    children = response.json().get("data")
-    if children.get("children") is None:
-        print(None)
-        return
-    i = 0
-    while i < 10:
-        for child in children.get("children"):
-            print(child.get("data").get("title"))
-        i += 1
+    posts = ""
+    if (type(subreddit) is not str):
+        posts = "None\n"
+    else:
+        api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {'user-agent': 'safari:holberton/0.1.0'}
+        response = requests.get(api_url, headers=headers)
+        if response.status_code is not 200:
+            posts = "None\n"
+        else:
+            posts_json = response.json().get("data").get("children")
+            for i in range(10):
+                post_title = posts_json[i].get("data").get("title")
+                posts += "{}\n".format(post_title)
+    print(posts, end="")
